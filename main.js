12c39b5fa5c54188e71ccaeffa67bd1adcd9fde0
cynikkk <theo.abel53@gmail.com>
2020-10-13 19:01:50 +0200

    Removed chrome + set .nav css class draggable

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
+    titleBarStyle: 'customButtonsOnHover',
+    frame : false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
